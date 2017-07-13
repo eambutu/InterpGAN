aa1fc309e5f7bf9de808d4762b4949f41c1203d4
Phillip <kip.phil@gmail.com>
2017-07-13 04:53:04 -0400

    Made some changes to model architecture

--- model.py
+++ model.py
@@ -27,8 +27,9 @@ def fc(input_dim, output_dim, bn=True):
 
 class Generator(nn.Module):
     """Generator containing 7 deconvolutional layers."""
-    def __init__(self, z_dim=256, image_w=256, image_h=112, conv_dim=32):
+    def __init__(self, z_dim=256, image_w=256, image_h=112, conv_dim=64):
         super(Generator, self).__init__()
+        self.conv_dim = conv_dim
         self.conv1 = conv(6, conv_dim, 4, bn=False)
         self.conv2 = conv(conv_dim, conv_dim*2, 4)
         self.conv3 = conv(conv_dim*2, conv_dim*4, 4)
@@ -38,7 +39,7 @@ class Generator(nn.Module):
         self.deconv3 = deconv(conv_dim*2, conv_dim, 4)
         self.deconv4 = deconv(conv_dim, 3, 4, bn=False)
 
-    def forward(self, z, frame_concat, image_w=256, image_h=112, conv_dim=32):
+    def forward(self, z, frame_concat, image_w=256, image_h=112):
         # let's ignore the z for now
         out = F.leaky_relu(self.conv1(frame_concat), 0.05)
         out = F.leaky_relu(self.conv2(out), 0.05)
@@ -54,19 +55,20 @@ class Discriminator(nn.Module):
     """Discriminator containing 4 convolutional layers."""
     def __init__(self, image_w=256, image_h=112, conv_dim=64):
         super(Discriminator, self).__init__()
-        self.conv1 = conv(9, conv_dim, 5, bn=False)
-        self.conv2 = conv(conv_dim, conv_dim*2, 5)
+        self.conv_dim = conv_dim
+        self.conv1 = conv(9, conv_dim, 5, pad=2, bn=False)
+        self.conv2 = conv(conv_dim, conv_dim*2, 5, pad=2)
         self.conv3 = conv(conv_dim*2, conv_dim*4, 3)
         self.conv4 = conv(conv_dim*4, conv_dim*8, 3)
         self.conv5 = conv(conv_dim*8, conv_dim*2, 3, stride=1)
         self.fc = fc(conv_dim * 2 * (image_w // 16) * (image_h // 16), 1)
 
-    def forward(self, x, image_w=256, image_h=112, conv_dim=32):                         # For image shape 256 x 112
+    def forward(self, x, image_w=256, image_h=112):                         # For image shape 256 x 112
         out = F.leaky_relu(self.conv1(x), 0.05)    # (?, 32, 128, 56)
         out = F.leaky_relu(self.conv2(out), 0.05)  # (?, 64, 64, 28)
         out = F.leaky_relu(self.conv3(out), 0.05)  # (?, 128, 32, 14)
         out = F.leaky_relu(self.conv4(out), 0.05)  # (?, 256, 16, 7)
         out = F.leaky_relu(self.conv5(out), 0.05)
-        out = out.view(-1, conv_dim * 2 * (image_w // 16) * (image_h // 16)) # (?, 28672)
+        out = out.view(-1, self.conv_dim * 2 * (image_w // 16) * (image_h // 16)) # (?, 28672)
         out = F.sigmoid(self.fc(out))
         return out
