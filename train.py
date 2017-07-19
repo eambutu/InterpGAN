dfbfbffd6628e542fe35a85a306aebd68c867122
Phillip <kip.phil@gmail.com>
2017-07-19 05:11:46 -0400

    Random and smooth labels

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import random
 import argparse
 import numpy as np
 import torch
@@ -11,22 +12,31 @@ from data import FramesDataset
 from model import Generator, Discriminator
 
 
+random.seed(1337)
+
 save_folder = './saves'
 d_steps = 2
 g_steps = 1
-d_learning_rate = 2e-4
-g_learning_rate = 2e-4
+d_learning_rate = 2e-5
+g_learning_rate = 2e-5
 z_dim = 256
 d_momentum = 0.9
 betas = (0.9, 0.999)
 num_epochs = 75
 batch_size = 4
+label_flip_prob = 0.05
 
 def get_distribution_sampler(mu, sigma):
     return lambda m, n: torch.Tensor(np.random.normal(mu, sigma, (m, n)))  # Gaussian
 
 def extract(v):
     return v.data.storage().tolist()
+
+def smooth_false_label():
+    return random.uniform(0, 0.3)
+
+def smooth_true_label():
+    return random.uniform(0.7, 1)
 
 def main():
     parser = argparse.ArgumentParser(description='Testing script')
@@ -51,14 +61,18 @@ def main():
 
     for epoch in range(num_epochs):
         for i_batch, sample_batched in enumerate(dataloader):
-            ones = torch.ones(batch_size)
-            zeros = torch.zeros(batch_size)
+            smooth_truth = torch.Tensor([smooth_true_label() for i in range(batch_size)])
+            smooth_false = torch.Tensor([smooth_false_label() for i in range(batch_size)])
             if args.gpu:
                 sample_batched['start_end_frames'] = sample_batched['start_end_frames'].cuda()
                 sample_batched['middle_frame'] = sample_batched['middle_frame'].cuda()
-                ones = ones.cuda()
-                zeros = zeros.cuda()
+                smooth_truth = smooth_truth.cuda()
+                smooth_false = smooth_false.cuda()
             start_end_frames = Variable(sample_batched['start_end_frames']).float()
+
+            # Whether or not to flip label for discriminator
+            flip_label = (random.random() < label_flip_prob)
+
             for d_index in range(d_steps):
                 D.zero_grad()
 
@@ -68,9 +82,6 @@ def main():
                 d_real_data = Variable(real_data)
                 d_real_data = d_real_data.float()
                 d_real_decision = D(d_real_data)
-                # 1 is true
-                d_real_error = criterion(d_real_decision, Variable(ones))
-                d_real_error.backward()
 
                 # Train D on fake
                 d_gen_input = Variable(g_sampler(4, z_dim))
@@ -79,8 +90,16 @@ def main():
                                                     d_fake_frame.data.double()), 1))
                 d_fake_frames = d_fake_frames.float()
                 d_fake_decision = D(d_fake_frames)
-                d_fake_error = criterion(d_fake_decision, Variable(zeros))
+
+                if not flip_label:
+                    d_real_error = criterion(d_real_decision, Variable(smooth_truth))
+                    d_fake_error = criterion(d_fake_decision, Variable(smooth_false))
+                else:
+                    d_real_error = criterion(d_real_decision, Variable(smooth_false))
+                    d_fake_error = criterion(d_fake_decision, Variable(smooth_truth))
+                d_real_error.backward()
                 d_fake_error.backward()
+
                 d_error = d_real_error + d_fake_error
                 d_optimizer.step()
 
@@ -93,7 +112,7 @@ def main():
                                                     g_fake_frame.data.double()), 1))
                 g_fake_frames = g_fake_frames.float()
                 dg_fake_decision = D(g_fake_frames)
-                g_error = criterion(dg_fake_decision, Variable(ones))
+                g_error = criterion(dg_fake_decision, Variable(smooth_truth))
 
                 g_error.backward()
                 g_optimizer.step()
